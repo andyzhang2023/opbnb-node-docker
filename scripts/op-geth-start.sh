754d6005a25e230ddd0c9fbedc0189af6d27c36f
andyzhang2023 <andy.zhangaiwen@gmail.com>
2024-07-25 08:18:36 +0800

    bsc mainnet node for evm parallel

--- scripts/op-geth-start.sh
+++ scripts/op-geth-start.sh
@@ -9,6 +9,19 @@ if [ "$NETWORK_NAME" == "mainnet" ]; then
   export NETWORK=opBNBMainnet
 fi
 
+GETH_DATA_DIR=$BEDROCK_DATADIR
+GETH_CHAINDATA_DIR="$GETH_DATA_DIR/geth/chaindata"
+
+if [ ! -d "$GETH_CHAINDATA_DIR" ]; then
+  echo "$GETH_CHAINDATA_DIR missing, running init"
+  echo "Initializing genesis."
+  geth init \
+    --datadir="$GETH_DATA_DIR" \
+    /genesis/genesis.json
+else
+  echo "$GETH_CHAINDATA_DIR exists."
+fi
+
 # Start op-geth.
 exec geth \
   --$NETWORK \
@@ -16,5 +29,29 @@ exec geth \
   --http.addr=0.0.0.0 \
   --authrpc.addr="0.0.0.0" \
   --authrpc.vhosts="*" \
+  --authrpc.port=8551 \
   --authrpc.jwtsecret=./jwt.txt \
-  --rollup.sequencerhttp=$L2_RPC
+  --rollup.sequencerhttp=$L2_RPC \
+  --verbosity=3 \
+  --http.corsdomain=* \
+  --http \
+  --http.api=eth,net,web3,txpool \
+  --http.vhosts=* \
+  --http.port=8545 \
+  --syncmode=full \
+  --metrics \
+  --metrics.port=6060 \
+  --metrics.addr=0.0.0.0 \
+  --allow-insecure-unlock \
+  --rpc.allow-unprotected-txs \
+  --gcmode=archive \
+  --txpool.globalslots=2000  \
+  --txpool.globalqueue=500  \
+  --txpool.accountqueue=64 \
+  --txpool.accountslots=16 \
+  --txpool.pricelimit=1 \
+  --txpool.nolocals=true \
+  --cache=6000 \
+  --cache.preimages \
+  --rollup.disabletxpoolgossip=false \
+  --bootnodes=enr:-KO4QHs5qh_kPFcjMgqkuN9dbxXT4C5Cjad4SAheaUxveCbJQ3XdeMMDHeHilHyqisyYQAByfdhzyKAdUp2SvyzWeBqGAYvRDf80g2V0aMfGhHFtSjqAgmlkgnY0gmlwhDaykUmJc2VjcDI1NmsxoQJUevTL3hJwj21IT2GC6VaNqVQEsJFPtNtO-ld5QTNCfIRzbmFwwIN0Y3CCdl-DdWRwgnZf,enr:-KO4QKIByq-YMjs6IL2YCNZEmlo3dKWNOy4B6sdqE3gjOrXeKdNbwZZGK_JzT1epqCFs3mujjg2vO1lrZLzLy4Rl7PyGAYvRA8bEg2V0aMfGhHFtSjqAgmlkgnY0gmlwhDbjSM6Jc2VjcDI1NmsxoQNQhJ5pqCPnTbK92gEc2F98y-u1OgZVAI1Msx-UiHezY4RzbmFwwIN0Y3CCdl-DdWRwgnZf
--- scripts/op-node-start.sh
+++ scripts/op-node-start.sh
@@ -15,4 +15,28 @@ exec op-node \
   --snapshotlog.file=./snapshot.log \
   --l2.jwt-secret=./jwt.txt \
   --l1=$OP_NODE__RPC_ENDPOINT \
-  --l2=http://op-geth:8551
+  --l2=http://op-geth:8551 \
+  --l1.trustrpc \
+  --sequencer.l1-confs=15  \
+  --verifier.l1-confs=15 \
+  --l1.http-poll-interval=3s  \
+  --l1.epoch-poll-interval=3s  \
+  --l1.rpc-max-batch-size=20 \
+  --rollup.config=/genesis/rollup.json \
+  --rpc.addr=0.0.0.0 \
+  --rpc.port=7000 \
+  --p2p.sync.req-resp  \
+  --p2p.listen.ip=0.0.0.0 \
+  --p2p.listen.tcp=9003 \
+  --p2p.listen.udp=9003 \
+  --p2p.bootnodes=enr:-J24QPSZMaGw3NhO6Ll25cawknKcOFLPjUnpy72HCkwqaHBKaaR9ylr-ejx20INZ69BLLj334aEqjNHKJeWhiAdVcn-GAYv28FmZgmlkgnY0gmlwhDTDWQOHb3BzdGFja4PMAQCJc2VjcDI1NmsxoQJ-_5GZKjs7jaB4TILdgC8EwnwyL3Qip89wmjnyjvDDwoN0Y3CCIyuDdWRwgiMs,enr:-J24QA9sgVxbZ0KoJ7-1gx_szfc7Oexzz7xL2iHS7VMHGj2QQaLc_IQZmFthywENgJWXbApj7tw7BiouKDOZD4noWEWGAYppffmvgmlkgnY0gmlwhDbjSM6Hb3BzdGFja4PMAQCJc2VjcDI1NmsxoQKetGQX7sXd4u8hZr6uayTZgHRDvGm36YaryqZkgnidS4N0Y3CCIyuDdWRwgiMs \
+  --metrics.enabled \
+  --metrics.addr=0.0.0.0 \
+  --metrics.port=7300 \
+  --rpc.enable-admin \
+  --p2p.ban.peers=false \
+  --p2p.scoring=none \
+  --log.level=debug \
+  --syncmode=execution-layer \
+  --l1.max-concurrency=20 \
+  --l2.skip-sync-start-check=true 
